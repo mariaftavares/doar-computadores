580c4098f34c26c65d70117cf1dd75b5ce9bddf2
Maria Fernanda Tavares <mariaffernandatavares@gmail.com>
2022-07-29 20:35:56 -0300

    Configurando váriaveis de ambiente

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv-safe').config();
 const routes = require('./routes/apiRoutes')
 app.use(cors());
 app.use(express.json());
