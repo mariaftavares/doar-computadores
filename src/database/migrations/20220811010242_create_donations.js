ab93fb8fa08dee7cd2eb95adbf8f661c3ffa85b8
Maria Fernanda Tavares <mariaffernandatavares@gmail.com>
2022-08-11 15:50:57 -0300

    Adicionando eslint e prettier

--- src/database/migrations/20220811010242_create_donations.js
+++ src/database/migrations/20220811010242_create_donations.js
@@ -2,32 +2,28 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema
-  .createTable('donations', (table)=> {
-      table.increments('iddonation').primary().unique();
-      table.string('name').notNullable();
-      table.string('email');
-      table.string('phone').notNullable();
-      table.string('zip').notNullable();
-      table.string('city').notNullable();
-      table.string('state').notNullable();
-      table.string('streetAddress').notNullable();
-      table.string('number').notNullable();
-      table.string('complement');
-      table.string('neighborhood').notNullable();
-      table.integer('deviceCount').notNullable().unsigned();
-      table.timestamps(true,true);
+exports.up = function (knex) {
+  return knex.schema.createTable('donations', (table) => {
+    table.increments('iddonation').primary().unique()
+    table.string('name').notNullable()
+    table.string('email')
+    table.string('phone').notNullable()
+    table.string('zip').notNullable()
+    table.string('city').notNullable()
+    table.string('state').notNullable()
+    table.string('streetAddress').notNullable()
+    table.string('number').notNullable()
+    table.string('complement')
+    table.string('neighborhood').notNullable()
+    table.integer('deviceCount').notNullable().unsigned()
+    table.timestamps(true, true)
   })
-  
-};
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-
+exports.down = function (knex) {
   return knex.schema.dropTable('donations')
-  
-};
+}
--- src/database/migrations/20220811013850_create_devices.js
+++ src/database/migrations/20220811013850_create_devices.js
@@ -2,29 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.up = function(knex) {
-  return knex.schema
-  .createTable('devices', (table)=> {
-      table.increments('id').primary().unique();
-      table.string('type').notNullable();
-      table.string('condition').notNullable();
-      table.integer('donations_iddonation').unsigned();
-      table.foreign('donations_iddonation')
-           .references('donations.iddonation')
-           .onDelete('CASCADE')
-           .onUpdate('CASCADE');
-      table.timestamps(true,true);
-      
+exports.up = function (knex) {
+  return knex.schema.createTable('devices', (table) => {
+    table.increments('id').primary().unique()
+    table.string('type').notNullable()
+    table.string('condition').notNullable()
+    table.integer('donations_iddonation').unsigned()
+    table.foreign('donations_iddonation').references('donations.iddonation').onDelete('CASCADE').onUpdate('CASCADE')
+    table.timestamps(true, true)
   })
-  
-};
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-
+exports.down = function (knex) {
   return knex.schema.dropTable('devices')
-  
-};
+}
