c524a9ea158872bc94a527014b35c1642458f80d
bruno <brunotheodoro123@gmail.com>
2022-08-11 10:41:26 -0300

    fix ajustes de campos: id

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
 const {resolve} = require('path')
 
 module.exports = {
-  client: 'mysql2',
-  connection: {
-    database: 'base_de_dados',
-    user: 'usuario',
-    password: 'senha'
-  },
+    client: 'mysql2',
+    connection: {
+      host : process.env.MYSQL_HOST,
+      user : process.env.MYSQL_USER ,
+      password : process.env.MYSQL_PASSWORD ,
+      database : process.env.MYSQL_DATABASE
+   },
     migrations: {
       directory: resolve(__dirname,'src', 'database','migrations')
   },
