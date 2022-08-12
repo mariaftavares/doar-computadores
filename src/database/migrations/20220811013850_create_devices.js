5caad4446104b5b06695045ffb9267c284238233
Maria Fernanda Tavares <mariaffernandatavares@gmail.com>
2022-08-11 23:18:34 -0300

    add Eslint e Prettier

--- src/database/migrations/20220811013850_create_devices.js
+++ src/database/migrations/20220811013850_create_devices.js
@@ -19,4 +19,4 @@ exports.up = function (knex) {
  */
 exports.down = function (knex) {
   return knex.schema.dropTable('devices')
-};
+}
--- src/database/migrations/20220811195636_fix_name_foredkey_devices.js
+++ src/database/migrations/20220811195636_fix_name_foredkey_devices.js
@@ -2,38 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
- exports.up =   function(knex) {
+exports.up = function (knex) {
   return knex.schema.alterTable('devices', (table) => {
-  
-    table.dropForeign('donations_iddonation','devices_donations_iddonation_foreign');
-    table.renameColumn('donations_iddonation','donation_id');
+    table.dropForeign('donations_iddonation', 'devices_donations_iddonation_foreign')
+    table.renameColumn('donations_iddonation', 'donation_id')
 
-    table.foreign('donation_id')
-         .references('donations.id')
-         .onDelete('CASCADE')
-         .onUpdate('CASCADE');
-    
+    table.foreign('donation_id').references('donations.id').onDelete('CASCADE').onUpdate('CASCADE')
   })
- 
-};
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down =  function(knex) {
-
+exports.down = function (knex) {
   return knex.schema.alterTable('devices', (table) => {
-  
-    table.dropForeign('donation_id','devices_donation_id_foreign');
-    table.renameColumn('donation_id','donations_iddonation');
+    table.dropForeign('donation_id', 'devices_donation_id_foreign')
+    table.renameColumn('donation_id', 'donations_iddonation')
 
-    table.foreign('donations_iddonation')
-        .references('donations.id')
-         .onDelete('CASCADE')
-         .onUpdate('CASCADE');
-  
-   
+    table.foreign('donations_iddonation').references('donations.id').onDelete('CASCADE').onUpdate('CASCADE')
   })
-  
-};
+}
